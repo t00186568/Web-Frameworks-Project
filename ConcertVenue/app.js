140b5d00d6a81809a588329f2d14ed36701200e4
t00186568 <kieran.mcclure@students.ittralee.ie>
2018-10-23 16:07:30 +0100

    Basic template created with Bootstrap
    Basic Bootstrap template created with NavBar and Jumbotron, beginning work on MongoDB Integration for Upcoming Events page

--- ConcertVenue/app.js
+++ ConcertVenue/app.js
@@ -7,6 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var mongoose = require('mongoose');
+
+
+
 var app = express();
 
 // view engine setup
@@ -21,6 +25,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+//Mongoose
+mongoose.connect('mongodb://localhost/concertVenue', {
+    useMongoClient: true
+});
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- ConcertVenue/routes/index.js
+++ ConcertVenue/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home - Concert Venue' });
+});
+
+router.get('/about', function(req, res, next){
+    res.render('about', { title: 'About - Concert Venue'});
+});
+
+router.get('/upcoming', function(req, res, next){
+    res.render('upcoming', { title: 'Upcoming Events - Concert Venue'});
+});
+
+router.get('/login', function(req, res, next){
+    res.render('login', { title: 'About - Concert Venue'});
 });
 
 module.exports = router;
