bf53eeb19903fc7050b6f96f4a3b23d50d7efb1a
t00186568 <kieran.mcclure@students.ittralee.ie>
2018-10-25 01:19:13 +0100

    Bypassed Login
    For now I have bypassed the login screen actually authenticating that a user exists (it now assigns a session regardless) until I can come up with a better solution. Login once and then go to upcoming events again and you will be allowed to proceed to the checkout.

--- ConcertVenue/routes/users.js
+++ ConcertVenue/routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/concertVenue');
 var User = require('../models/users');
 //Login in code from tutorial: https://www.youtube.com/watch?v=pzGQMwGmCnc
 //and https://www.youtube.com/watch?v=zsOGmMuwhT4
@@ -9,40 +10,31 @@ router.get('/login', function(req, res, next){
   res.render('login', { title: 'About - Concert Venue'});
 });
 
-router.post('/login', function(req, res){
-  var username = req.body.username;
-  var password = req.body.password;
- 
-  User.findOne({'username': username, 'password': password}, function(err, user){
+//Get all users
+router.get('/', function(req,res,next){
+ User.find({}, function(err, users){
     if(err){
-      console.log(err);
-      return res.status(500).send();
-    } else if(!user){
-      
-      return res.status(401).send();
-      
-
-    } else{
-    
-    req.session.user = user;
-    
-    return res.redirect("/");
-    
+      res.send('Error');
+      next();
     }
-  
+    res.json(users);
+    
   });
-   
 });
 
-router.get('/dashboard', function(req, res ){
-  if(!req.session.user){
-    return res.status(401).send();
-  }
-  
-
-  res.render('dashboard');
+//Delete user by ID
+router.delete('/:id', function(req,res){
+  User
+    .findByIdAndRemove(req.params.id)
+    .exec()
+    .then(doc => {
+      if(!doc){ return res.status(404).end();}
+      return res.send("Record Deleted");
+    })
+    .catch(err => next(err));
 });
 
+//Post - register new user
 router.post('/register', function(req, res){
   var username = req.body.username;
   var password = req.body.password;
@@ -63,6 +55,41 @@ router.post('/register', function(req, res){
       return res.status(200).send();
   });
 });
+
+
+
+//Login 
+router.post('/login', function(req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+ //Temporarily authenticating user all the time while I try to figure out Login
+  User.find({'username': username, 'password': password}, function(err, user){
+    // if(err){
+    //   console.log(err);
+    //   return res.status(500).send();
+    // } else if(!user){
+      
+    //   return res.status(401).send();
+      
+
+    // } else{
+    
+    req.session.user = user;
+    
+    return res.redirect("/");
+    
+    //}
+  
+  });
+   
+});
+
+
+
+
+
+
+
 
 
 module.exports = router;
